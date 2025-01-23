088108dadc555fd3eb38b7e4eca99663e6db15d2
Mayank <mayankpatil2605@gmail.com>
2025-01-22 21:56:12 -0800

    Added ChakraProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-createRoot(document.getElementById('root')!).render(
+const theme = extendTheme({
+  config:{
+    initialColorMode: "light", // Default mode is light
+    useSystemColorMode: false, // Disable automatic system-based mode
+  }
+})
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
